e5be145792a347e190a389a5eb76560760e5f8bf
NehuenCuenca <nehuen.cuenca@hotmail.com>
2023-08-15 12:12:57 -0300

    first commit, update readme, moving files to a correspondient directory, cleaned the app.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Este es el app
+  </div>
+</template>
+
+<script>
+// import { reactive, toRefs } from 'vue'
+
+export default {
+  setup () {
+  
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
